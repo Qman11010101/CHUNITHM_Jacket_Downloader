58014b41532d8a9e657bd724eadb8e411b579044
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2022-03-22 23:15:15 +0900

    initial

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+import requests
+
+req_url = "https://chunithm.sega.jp/storage/json/music.json"
+img_baseurl = "https://new.chunithm-net.com/chuni-mobile/html/mobile/img/"
+
+# jacketpath = "~/apps/chunijacket/"
+jacketpath = "./"
+
+data = requests.get(req_url).json()
+
+choiced = []
+for music in data:
+    if music["lev_mas"] != "":
+        choiced.append(music)
+
+os.makedirs("jackets", exist_ok=True)
+
+for music in choiced:
+    try:
+        url = img_baseurl + music["image"]
+        title = music["title"]
+        table = str.maketrans('\\/:*?"<>|#', "__________", "")
+        filepath = jacketpath + "jackets/" + f"{title}.png".translate(table)
+        if not os.path.isfile(filepath):
+            imageblob = requests.get(url)
+            if imageblob.status_code == 200:
+                with open(filepath, "wb") as i:
+                    i.write(imageblob.content)
+            time.sleep(1)
+    except:
+        time.sleep(1)
