01a40cd9ffd6cf47023d63365601bb80d2702290
Kjuman Enobikto <30486982+Qman11010101@users.noreply.github.com>
2022-03-24 20:21:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import time
+import traceback
 
 import requests
 
@@ -24,10 +25,14 @@ for music in choiced:
         table = str.maketrans('\\/:*?"<>|#', "__________", "")
         filepath = jacketpath + "jackets/" + f"{title}.png".translate(table)
         if not os.path.isfile(filepath):
+            print(f"「{title}」を{url}からダウンロードしています")
             imageblob = requests.get(url)
             if imageblob.status_code == 200:
                 with open(filepath, "wb") as i:
                     i.write(imageblob.content)
             time.sleep(1)
+        else:
+            print(f"「{title}」は既に存在しています")
     except:
+        traceback.print_exc()
         time.sleep(1)
